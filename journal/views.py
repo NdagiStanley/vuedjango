add2c2470ab327531b136d4c63c621815517032a
Stanley Ndagi <ndagis@gmail.com>
2017-09-07 19:39:51 +0300

    Implement VueJS

--- journal/views.py
+++ journal/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def index(request):
+    # The content of context is rendered in the templates
+    # using {{ }} delimiters
     context = {
         'days': [1, 2, 3],
     }
