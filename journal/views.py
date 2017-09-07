6e8f744bfd5c00ff2e1cd198adfa088e424d5b9e
Stanley Ndagi <ndagis@gmail.com>
2017-09-07 18:15:04 +0300

    Add views using Jinja Templates

--- journal/views.py
+++ journal/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    context = {
+        'days': [1, 2, 3],
+    }
+    return render(request, 'days.html', context)
